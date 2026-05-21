8db8679a91d14f7c9ec3006d231290bbaebe39ef
calmespectre <gerrad.meitameisaruni@gmail.com>
2026-05-21 10:55:14 -0700

    adjusted main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default defineConfig({
+  plugins: [react()],   // ✅ OK – each plugin is a fresh function call
+})
